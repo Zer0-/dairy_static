8a4ec9c23005a11ba7b173f04bb75733e020ff3d
Philipp Volguine <phil.volguine@gmail.com>
2022-11-19 17:54:49 -0500

    set up basic grid

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
+
+export SASS_PATH=node_modules/flexboxgrid-sass:node_modules/breakpoint-sass/stylesheets
+
+./node_modules/.bin/sass src:target
+
 cat ./src/head.html > ./target/homepage.html
