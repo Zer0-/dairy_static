09986fac1d78d233c58f3184e1019d25e89e27be
Philipp Volguine <phil.volguine@gmail.com>
2022-11-19 23:59:22 -0500

    Style the rest of the pages

--- build.sh
+++ build.sh
@@ -8,5 +8,16 @@ export SASS_PATH=node_modules/flexboxgrid-sass:node_modules/breakpoint-sass/styl
 ./node_modules/.bin/sass src:target
 
 cat ./src/head.html > ./target/homepage.html
+cat ./src/homepage.html >> ./target/homepage.html
+cat ./src/footer.html >> ./target/homepage.html
+
+cat ./src/head.html > ./target/sanitizer.html
+cat ./src/sanitizer.html >> ./target/sanitizer.html
+cat ./src/footer.html >> ./target/sanitizer.html
+
+cat ./src/head.html > ./target/specialty_alcohols.html
+cat ./src/specialty_alcohols.html >> ./target/specialty_alcohols.html
+cat ./src/footer.html >> ./target/specialty_alcohols.html
+
 cp -v ./src/acumin-pro* ./target
 cp -rv ./src/img ./target
