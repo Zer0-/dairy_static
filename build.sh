16a670b57ecc1d8feedb6098bcc0d6c9df430f41
Philipp Volguine <phil.volguine@gmail.com>
2022-11-19 21:45:29 -0500

    styling... sizes aren't perfect on mobile

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ export SASS_PATH=node_modules/flexboxgrid-sass:node_modules/breakpoint-sass/styl
 ./node_modules/.bin/sass src:target
 
 cat ./src/head.html > ./target/homepage.html
+cp -v ./src/acumin-pro* ./target
+cp -rv ./src/img ./target
